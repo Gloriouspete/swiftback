5d2bb03a3338ca58061d54e894790ec7d8274d02
Glö <105151046+Gloriouspete@users.noreply.github.com>
2024-07-09 22:01:56 +0100

    Update message.js

--- services/message.js
+++ services/message.js
@@ -1,19 +1,29 @@
-const Message = require("../model/message.js")
-const Addmessage = (data) => {
-    console.log('it got here',data)
+const Message = require("../model/message.js");
+const User = require("../model/user.js");
+
+const Addmessage = async (data) => {
+  console.log('it got here', data);
   const { chatid, sender, receiver, type, message } = data;
+
   try {
+    const receiverData = await User.findOne({ userid: receiver });
+    if (receiverData && receiverData.block.includes(sender)) {
+      return console.log('Sender is blocked by receiver.');
+    }
+
     const newMessage = new Message({
       chatid,
       sender,
       receiver,
       type,
-      message
-    })
-    console.log(newMessage)
-    newMessage.save()
+      message,
+    });
+
+    console.log(newMessage);
+    await newMessage.save();
+    console.log('Message saved successfully.');
   } catch (error) {
-    console.log(error);
+    console.log('Error saving message:', error);
   }
 };
 
